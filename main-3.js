4bdb96778f5207573df4a1310f439419af9222af
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-06-30 11:22:06 +0900

    Plotly : Range Slider
    Time series

--- main-3.js
+++ main-3.js
@@ -1,9 +1,67 @@
-var data = [
-    {
-      x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-      y: [1, 3, 6],
-      type: 'scatter'
-    }
-  ];
-  
-  Plotly.newPlot('myDiv', data,{edited: true});
+const rawDataURL = 'https://raw.githubusercontent.com/plotly/datasets/master/2016-weather-data-seattle.csv';
+const xField= 'Date';
+const yField= 'Mean_TemperatureC';
+
+const selectorOptions = {
+    buttons: [{
+        step: 'month',
+        stepmode: 'backward',
+        count: 1,
+        label: '1m'
+    }, {
+        step: 'month',
+        stepmode: 'backward',
+        count: 6,
+        label: '6m'
+    }, {
+        step: 'year',
+        stepmode: 'todate',
+        count: 1,
+        label: 'YTD'
+    }, {
+        step: 'year',
+        stepmode: 'backward',
+        count: 1,
+        label: '1y'
+    }, {
+        step: 'all',
+    }],
+};
+
+Plotly.d3.csv(rawDataURL, (error,rawData) =>{
+    if(error) throw error;
+
+    const data = prepData(rawData);
+    const layout = {
+        title: 'Time series with range slider and selectors',
+        xaxis: {
+            rangeselector: selectorOptions,
+            rangeslider: {}
+        },
+        yaxis: {
+            fixedrange: true
+        }
+    };
+
+    Plotly.newPlot('myDiv', data, layout);
+
+})
+
+
+//function to make data format
+function prepData(rawData) {
+    const x = [];
+    const y = [];
+
+    rawData.forEach((datum, i) => {
+
+        x.push(new Date(datum[xField]));
+        y.push(datum[yField]);
+    });
+
+    return [{
+        mode: 'lines',
+        x: x,
+        y: y
+    }];
+}
